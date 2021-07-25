392c39c698647969d7d8a2275601bdebffcd9511
kasilakeconsulting <87913200+kasilakeconsulting@users.noreply.github.com>
2021-07-25 14:58:11 -0400

    Coding revisions to flesh things out.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # excel-python-test
-Test for working with Excel in Python using Pycharm
+Tests for working with Excel (.xls) in Python using Pycharm.
+
+Developed in Pycharm CE 2021 with Python 3.8 on macOS.
+
+Requires the package xlrd.
--- main.py
+++ main.py
@@ -1,38 +1,57 @@
 '''
-Open an Excel file and read content from certain cells.
+Open an Excel file, read and output the existing values, update some values and save, then output the revised values.
 '''
 
 import xlrd
-import platform
-import os
 from os import path
 
-print("-----\n\n")
 
-nodename = platform.node()
+def output_bal_and_sum(sheetname):
+    workbook = xlrd.open_workbook(filename)
+
+    sheet = workbook.sheet_by_name(sheetname)
+    # You can also reference a sheet by index:
+    # sheet = workbook.sheet_by_index(0)
+
+    # Loop through the sheet.
+    print("   Number of rows and columns: ", sheet.nrows, ",", sheet.ncols)
+    i = 2
+    # Skip the first 2 header rows.
+    maxRows = sheet.nrows - 2
+
+    while i <= maxRows:
+        balance = sheet.cell_value(i, 2)
+        # Check if the custID is not blank; if it is, skip the row
+        # (Don't check the balance value for blank because 0.0 is a valid balance but it's considered blank.)
+        if sheet.cell_value(i, 0):
+            if balance and balance < 0:
+                comment = " *** below zero"
+            else:
+                comment = ""
+            print("  ", sheet.cell_value(i, 1), ":", balance, comment)
+        i += 1
+
+    print("   Sum: ", sheet.cell_value(1, 5))
+
+
+print("\n-----\n")
 
 filename = "excel1.xls"
-
-sheetname = "Sheet 1"
-
-print("os: " + os.name)
-print(os.uname())
-print('system: ' + platform.system())
-print("current directory: " + path.curdir)
-print("default path: " + path.defpath)
-print("file path: " + path.realpath(filename))
-
-print("\nfile exists? " + str(path.exists(filename)))
+filePath = path.realpath(filename)
+print("File path: " + filePath)
 
 if path.exists(filename) is False:
-    print("*** Check if the file needs to be downloaded from the cloud")
+    print("*** File does not exist: " + filePath)
 else:
-    print("is a file? " + str(path.isfile(filename)))
+    if path.isfile(filename) is False:
+        print("*** This is not a file: " + filePath)
+    else:
+        print("\nExisting sheet values:\n")
+        output_bal_and_sum("Sheet1")
 
-    workbook = xlrd.open_workbook(filename)
-    # sheet = workbook.sheet_by_name(sheetname)
-    sheet = workbook.sheet_by_index(0)
+        # Update some values and save.
 
-    print (sheet.cell_value(0, 0))
-    print (sheet.cell_value(1, 0))
-    print (sheet.cell_value(2, 0))
+
+
+        print("\nRevised sheet values:\n")
+        output_bal_and_sum("Sheet1")
